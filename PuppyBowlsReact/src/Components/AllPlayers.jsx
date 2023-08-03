4376e9988e96de65d719edcf232bfa7f7b2f897a
phanhkn <phanhkn@gmail.com>
2023-08-03 18:51:40 -0400

    added things

---
+++ PuppyBowlsReact/src/Components/AllPlayers.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function AllPlayers() {
+  return (
+    <>
+      <div>
+        <h1>All players</h1>
+      </div>
+    </>
+  );
+}
+
+export default AllPlayers;
---
+++ PuppyBowlsReact/src/Components/SinglePlayer.jsx
@@ -0,0 +1,11 @@
+function SinglePlayer() {
+  return (
+    <>
+      <div>
+        <h1>Single Players</h1>
+      </div>
+    </>
+  );
+}
+
+export default SinglePlayer;
